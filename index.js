b8e6c56d05e8d8c4b92137dd6488cc87cf125d7a
Alen N <nela.asus+dvcs@gmail.com>
2018-12-06 23:53:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+// define add function
+var add = (a,b) => a + b;
+
+// subtract function
+var subtract = (a,b) => a - b;
+
+// multiply function
+var multiply = (a,b) => a * b;
+
+// divide function
+var divide = (a,b) => a / b;
+
+// post increment function
+var inc = (n) => { n++; return n };
+
+// post decrement function
+var dec = (n) => { n--; return n };
+
+// parseInt function
+var makeInt = (n) => parseInt(n, 10);
+
+// parseFloat function
+var preserveDecimal = (n) => parseFloat(n);
